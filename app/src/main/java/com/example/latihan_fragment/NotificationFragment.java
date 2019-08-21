761b9630f74748896fe6dff964261d114addc709
nurcahya2442 <nurcahya2443@gmail.com>
2019-08-21 15:17:37 +0700

    PWPB Latihan Fragment

---
+++ app/src/main/java/com/example/latihan_fragment/NotificationFragment.java
@@ -0,0 +1,18 @@
+package com.example.latihan_fragment;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NotificationFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_notification_fragment, null);
+    }
+}
